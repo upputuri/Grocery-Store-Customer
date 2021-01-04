ee87d36da6c1e53f05f902731092191af401cd1c
Srikanth Upputuri <usrikanth@gmail.com>
2021-01-04 15:59:17 +0530

    Stripe POC implementation

--- customer/src/main/java/com/dolittle/ecom/customer/payments/StripeService.java
+++ customer/src/main/java/com/dolittle/ecom/customer/payments/StripeService.java
@@ -1,5 +1,8 @@
 package com.dolittle.ecom.customer.payments;
 
+import javax.annotation.PostConstruct;
+
+import com.google.gson.JsonSyntaxException;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
@@ -7,33 +10,52 @@ import com.stripe.param.checkout.SessionCreateParams;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.stripe.model.StripeObject;
+import com.stripe.net.ApiResource;
+import com.stripe.model.Event;
+import com.stripe.model.EventDataObjectDeserializer;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.PaymentMethod;
+
+import com.google.gson.JsonSyntaxException;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component(value = "stripe")
+@RestController
 public @Data class StripeService implements PGIService {
 
     private final String id = "stripe";
 
-    // @Value("${stripekey}")
-    private String key;
+    @Value("${stripekey}")
+    private String key; 
 
-    // @Value("${stripesecret}")
+    @Value("${stripesecret}")
     private String secret;
+
+    @PostConstruct
+    public void init() {
+      Stripe.apiKey = secret;
+    }
 
     @Override
     public String startTransaction(int amount, String transactionId) {
-        Stripe.apiKey = secret;
         try {
             SessionCreateParams params =
             SessionCreateParams.builder()
               .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
               .setMode(SessionCreateParams.Mode.PAYMENT)
-              .setSuccessUrl("https://example.com/success")
-              .setCancelUrl("https://example.com/cancel")
+              .setSuccessUrl("http://localhost:8100/support")
+              .setCancelUrl("http://localhost:8100/login")
               .addLineItem(
               SessionCreateParams.LineItem.builder()
                 .setQuantity(1L)
@@ -64,6 +86,49 @@ public @Data class StripeService implements PGIService {
     @Override
     public int validatePaymentResponse(String order, String response) {
         return 0;
+    }
+
+    @PostMapping(value = "/stripeevents", produces = "application/hal+json")
+    public void handleStripeWebHookEvents(@RequestBody String payload)
+    {
+        Event event = null;
+
+        try {
+            event = ApiResource.GSON.fromJson(payload, Event.class);
+        } catch (JsonSyntaxException e) {
+          // Invalid payload
+          throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+      
+        // Deserialize the nested object inside the event
+        EventDataObjectDeserializer dataObjectDeserializer = event.getDataObjectDeserializer();
+        StripeObject stripeObject = null;
+        if (dataObjectDeserializer.getObject().isPresent()) {
+          stripeObject = dataObjectDeserializer.getObject().get();
+        } else {
+          // Deserialization failed, probably due to an API version mismatch.
+          // Refer to the Javadoc documentation on `EventDataObjectDeserializer` for
+          // instructions on how to handle this case, or return an error here.
+        }
+      
+        // Handle the event
+        switch (event.getType()) {
+          case "checkout.session.completed":
+            Session paymentSession = (Session) stripeObject;
+            System.out.println("Payment session complete!");
+            break;
+          case "payment_intent.succeeded":
+            PaymentIntent paymentIntent = (PaymentIntent) stripeObject;
+            System.out.println("PaymentIntent was successful!");
+            break;
+          case "payment_method.attached":
+            PaymentMethod paymentMethod = (PaymentMethod) stripeObject;
+            System.out.println("PaymentMethod was attached to a Customer!");
+            break;
+          // ... handle other event types
+          default:
+            System.out.println("Unhandled event type: " + event.getType());
+        }
     }
     
 }
