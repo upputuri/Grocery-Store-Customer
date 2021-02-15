f13f8ef1682875cb41a6d12084335bdfe4823dfa
Srikanth Upputuri <usrikanth@gmail.com>
2021-02-15 08:31:01 +0530

    cleanup cors config, disable spring session

--- customer/src/main/java/com/dolittle/ecom/app/WebSecurityConfig.java
+++ customer/src/main/java/com/dolittle/ecom/app/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -44,6 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         AuthenticationEntryPoint authenticationEntryPoint = new GrocAuthEntryPoint();
         http.csrf().disable().cors().and()
+                                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                                 .authorizeRequests().antMatchers("/me").authenticated()
                                 .antMatchers("/customers/*/cart/items").authenticated()
                                 .antMatchers("/customers/*/cart").authenticated()
